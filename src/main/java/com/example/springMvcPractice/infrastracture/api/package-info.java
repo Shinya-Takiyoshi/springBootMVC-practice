545e6f421384a1c91a188e50302300d0dba471b7
Shinya-Takiyoshi <garo.savior@gmail.com>
2023-04-10 17:13:52 +0900

    first commit

---
+++ src/main/java/com/example/springMvcPractice/infrastracture/api/package-info.java
@@ -0,0 +1,1 @@
+package com.example.springMvcPractice.infrastracture.api;
---
+++ src/main/java/com/example/springMvcPractice/web/MainController.java
@@ -0,0 +1,17 @@
+package com.example.springMvcPractice.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index(Model model){
+
+        return "こんにちは";
+    }
+}
